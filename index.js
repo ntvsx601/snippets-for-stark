a063267d84e6c06c4711b2c151f896cdff6ae747
ntvsx601 <55246709+ntvsx601@users.noreply.github.com>
2023-12-26 01:31:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,53 +2,49 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { Account, ec, number, Provider } from "starknet";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
+
+// Logging Middleware
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const transferFunds = async (senderAddress, privateKey, contractAddress, recipient, amount) => {
-    console.log('---- start ----');
-    
-    const txnInvocation = {
-        contractAddress,
-        entrypoint: 'transfer',
-        calldata: [recipient, amount, "0"]
-    };
+// Helper Functions
+async function transferFunds(senderAddress, privateKey, contractAddress, recipient, amount) {
+    // ... existing transferFunds function ...
+}
 
-    const maxFee = number.toBN('440005097511309');
-    console.log(maxFee);
-
-    const senderKeyPair = ec.getKeyPair(privateKey);
-    const account = new Account(new Provider(), senderAddress, senderKeyPair);
-
-    try {
-        const txhash = await account.execute(txnInvocation, undefined, { maxFee });
-        console.log(txhash);
-        console.log('---- end ----');
-        return "transfer submitted";
-    } catch (error) {
-        console.error("Error during transfer:", error);
-        throw error;
-    }
+// Validation Middleware
+const validateTransferInput = (req, res, next) => {
+    // Add validation logic here
+    next();
 };
 
-app.post('/transfer', async (req, res) => {
-    const senderAddress = '0x70bdb4da1c7153682bd9760f3cb297addb3a15e30cc77be7194434ebaa24ad9';
-    const privateKey = '1188213934273022935890293708434664722187072329437599921063646353986905980672';
-    const contractAddress = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7';
-    const recipient = "0x0535377e2fA496930E0358Ae7ddFDbb5aBEaAeB4EC431bdff518BC5D3150b2B";
-    const amount = "10";
-
-    try {
-        const responseMessage = await transferFunds(senderAddress, privateKey, contractAddress, recipient, amount);
-        res.send(responseMessage);
-    } catch (error) {
-        res.status(500).send("Error in processing transfer");
-    }
+// Routes
+app.post('/transfer', validateTransferInput, async (req, res, next) => {
+    // ... existing transfer route ...
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+app.get('/balance/:address', async (req, res, next) => {
+    const { address } = req.params;
+    // Implement logic to check balance
+    res.send(`Balance for ${address}: [balance here]`);
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
